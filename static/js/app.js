abac8e6049e4c4a66859b78f034bcb8a0163785d
lllohr <98861909+lllohr@users.noreply.github.com>
2022-04-24 21:55:35 -0700

    update all files for project

---
+++ static/js/app.js
@@ -0,0 +1,13 @@
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    tbody.html("");
+  }
+  
+  
+  data.forEach((dataRow) => {
+
+});
